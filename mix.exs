85bff2b60b5de0e281a3fc36fa4bb4c632eecd35
Eduardo Borsa <borsa.dado@gmail.com>
2020-07-25 20:18:32 -0300

    Added changelog and License
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Blog.MixProject do
   def project do
     [
       app: :blog,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: "0.0.1",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
