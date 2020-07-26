498325cd5e9b75495280430c310595c867313e66
Eduardo Borsa <borsa.dado@gmail.com>
2020-07-26 18:23:55 -0300

    Automated CD (#13)
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+echo "Start gialixir deploy"
+
+git remote add gigalixir https://borsa.dado%40gmail.com:b0a2f65f-0801-4323-895f-b1978bc1e057@git.gigalixir.com/blogeduardodeply.git
+git push gigalixir master
+
+echo "Finished Deploy"
